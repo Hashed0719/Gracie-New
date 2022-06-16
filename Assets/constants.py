1523901edb23d0ae3daf27220c1a42531173d1be
Hash <drivelecture@gmail.com>
2022-06-16 19:15:20 +0530

    constants.py updated

--- Assets/constants.py
+++ Assets/constants.py
@@ -1,10 +1,23 @@
 import disnake
 
 client_version = disnake.__version__
-guild_id = 917821802351308910
+guild_id = 946579456791818281
 
 class channel_id:
-  audit = 944094115543535726
-  confess = 944096558142586880
+  audit = 986961201718239253
+  confess = 986961201718239253
   gracie_bot = 923716595740713010
   bot_status = 950181434017062942
+
+playlist = 'https://open.spotify.com/playlist/7Mven07omZONK1nS8o9oEo?si=3cba33f71db04ccc'
+intro_playlist = 'https://open.spotify.com/playlist/4Uigwu6pJOSuOLDE0jkFQf?si=008e260ad78d4583'
+discography_playlist = 'https://open.spotify.com/playlist/7Mven07omZONK1nS8o9oEo?si=40353f8e00da445b'
+server_link = 'https://discord.gg/gracieabrams'
+social_links =  {
+  "Instagram":"https://www.instagram.com/gracieabrams",
+  "YouTube":"https://www.youtube.com/c/GracieAbrams",
+  "Tiktok":"https://tiktok.com/@tehegracietehe",
+  "Twitter":"https://twitter.com/gracieabrams",
+  "Facebook":"https://www.facebook.com/gracieabrams",
+  "SoundCloud":"https://soundcloud.com/gracie-abrams"
+}
