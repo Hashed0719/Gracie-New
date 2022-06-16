b0991bb72438cb8494b61ffd25ff75808b6721be
Hash <drivelecture@gmail.com>
2022-06-16 09:05:51 +0530

    added custom prefix callable and on_message event for bot prefix on ping.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import __version__
+print(__version__)
 
 from keep_alive import keep_alive
 
@@ -13,10 +14,24 @@ import os
 import logging 
 
 
-print(__version__)
+BOT_PREFIX = ";"
+
+def _get_prefix(bot: commands.Bot, message: discord.Message):
+    first_content = message.content.split()[0]
+    commands_invoked = [
+        command for command in bot.commands 
+        if command.name.lower() in first_content.lower()
+        or any(alias in first_content.lower() for alias in command.aliases)
+    ]
+    for command in commands_invoked:
+        if hasattr(command.cog, "prefix"):
+            if command.cog.prefix + command.name.lower() == first_content.lower():
+                return command.cog.prefix 
+        else:
+            return BOT_PREFIX
 
 bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or(";"),
+    command_prefix=_get_prefix,
     activity=discord.Activity(
         type=discord.ActivityType.watching,
         name="you half-drunk happy"
@@ -28,6 +43,13 @@ bot = commands.Bot(
 @bot.event
 async def on_ready():
     print(f"Succesfully connected as {bot.user}")
+
+@bot.event
+async def on_message(msg: discord.Message):
+    if f"<@{bot.user.id}>" in msg.content:
+        await msg.reply(f"Hello there! My prefix is `{BOT_PREFIX}`")
+    else:
+        await bot.process_commands(msg)
 
 extensions = [
     "Cogs._initManager",
