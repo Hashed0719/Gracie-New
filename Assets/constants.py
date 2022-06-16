9589c4c8938c9c4d713ac67eaf0d02d45bff489d
Hash <drivelecture@gmail.com>
2022-06-16 19:18:17 +0530

    minor updates in constants.py

--- Assets/constants.py
+++ Assets/constants.py
@@ -1,11 +1,11 @@
 import disnake
 
 client_version = disnake.__version__
-guild_id = 946579456791818281
+guild_id = 917821802351308910
 
 class channel_id:
-  audit = 986961201718239253
-  confess = 986961201718239253
+  audit = 919947326842347520
+  confess = 944096558142586880
   gracie_bot = 923716595740713010
   bot_status = 950181434017062942
 
