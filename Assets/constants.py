6030057c0dc15ad82ec5e747579806a67f6572b4
Hash <drivelecture@gmail.com>
2022-06-18 11:05:55 +0530

    changes in constants.py for log channel 'audit logs -> Gracie logs'

--- Assets/constants.py
+++ Assets/constants.py
@@ -4,7 +4,7 @@ client_version = disnake.__version__
 guild_id = 917821802351308910
 
 class channel_id:
-  audit = 919947326842347520
+  audit = 944094115543535726
   confess = 944096558142586880
   gracie_bot = 923716595740713010
   bot_status = 950181434017062942
